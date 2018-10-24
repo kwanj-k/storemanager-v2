dd2c158f02c0995be65d328cf55b08b7c61f6cfe
kwanj-k <mwangikelvin23@.com>
2018-10-25 00:00:51 +0300

    [Feature #161404710] Add .travis.sh file to configure db

---
+++ .travis.sh
@@ -0,0 +1,5 @@
+export MODE='testing'
+export SECRET_KEY="secretkeymyfootiknowyoucanseeit"
+export FLASK_APP="run.py"
+export FLASK_DEBUG=1
+export Test_URL="dbname=tch3 user=kwanj password=4084theG"
