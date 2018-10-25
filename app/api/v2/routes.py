470fb7dd102a60678c4b4cc356b41a54db68eaa5
kwanj-k <mwangikelvin23@.com>
2018-10-25 16:54:01 +0300

    [Feature #161404774] Add category and sales

--- app/api/v2/routes.py
+++ app/api/v2/routes.py
@@ -12,6 +12,8 @@ from .views.auth import v2 as newstore_route
 from .views.auth import u2 as login_route
 from .views.products import v2 as products_route
 from .views.carts import v2 as cart_routes
+from .views.categories import v2 as category_routes
+from .views.sales import v2 as sales_routes
 
 authorizations = {
     'apikey': {
@@ -31,3 +33,5 @@ api.add_namespace(newstore_route, path="/signup")
 api.add_namespace(login_route, path="/")
 api.add_namespace(products_route, path="/products")
 api.add_namespace(cart_routes, path="/cart")
+api.add_namespace(category_routes, path="/categories")
+api.add_namespace(sales_routes, path="/sales")
---
+++ app/api/v2/views/categories.py
@@ -0,0 +1,149 @@
+"""
+This file contains all the categories resources/endpoints
+i.e get/put/delete/post and a category to product
+"""
+
+
+#Third party imports
+from flask_restplus import Resource
+from flask import request,abort
+from flask_jwt_extended import  get_jwt_identity,jwt_required
+
+#Local imports
+from app.api.v2.models.category import Category
+from app.api.v2.db_config import cur
+from app.api.v2.views.expect import CategoryEtn
+from app.api.v2.db_config import conn
+from .helpers import get_user_by_email,get_store_id
+from app.api.common.validators import category_validator,admin_required
+
+new_cat = CategoryEtn().categories
+v2 = CategoryEtn.v2
+
+
+@v2.route('')
+class Categories(Resource):
+    @v2.doc( security='apikey')
+    @jwt_required
+    @admin_required
+    @v2.expect(new_cat)
+    def post(self):
+        """
+        Create a category
+        """
+        json_data = request.get_json(force=True)
+        category_validator(json_data)
+        store_id = get_store_id(get_jwt_identity())
+        cur.execute("SELECT * FROM categories WHERE name='{}';".format(json_data['name']))
+        category = cur.fetchone()
+        if category and category[1] == store_id:
+            msg = 'Category already exists'
+            abort(406, msg)
+        name = json_data['name']
+        new_cat = Category(store_id,name)
+        new_cat.add_category()
+        return {"status":"Success!","Category":new_cat.json_dump()},201
+
+
+    @v2.doc( security='apikey')
+    @jwt_required
+    def get(self):
+        """
+        Get all get categories
+        """
+        store_id = get_store_id(get_jwt_identity())
+        cur.execute("SELECT * FROM categories WHERE store_id='{}';".format(store_id))
+        categories = cur.fetchall()
+        if len(categories) < 1:
+            msg = 'There are no categories at this time'
+            abort(404, msg)
+        all_categories = []
+        for c in categories:
+            format_cat = {
+            "name":c[2],
+            "Added_at":c[3]
+            }
+            all_categories.append(format_cat)
+        return {"status":"Success!","categories":all_categories},200
+
+@v2.route('/<int:id>')
+class CategoryDetail(Resource):
+    @v2.doc( security='apikey')
+    @jwt_required
+    @admin_required
+    @v2.expect(new_cat)
+    def put(self,id):
+        """
+        Update a category
+        """
+        json_data = request.get_json(force=True)
+        category_validator(json_data)
+        cur.execute("SELECT * FROM categories WHERE id={};".format(id))
+        category = cur.fetchone()
+        store_id = get_store_id(get_jwt_identity())
+        if not category or category[1] != store_id:
+            msg = {"message":'Category does not exist'},404
+            return msg
+        name = category[2]
+        cur.execute("UPDATE categories SET name='{}' WHERE id ={}".format(name,id))
+        conn.commit()
+        cur.execute("SELECT * FROM categories WHERE id={};".format(id))
+        new_c = cur.fetchone()
+        format_new_c ={
+            "category_name":new_c[2]
+        }
+        return {"status":"Updated!","category":format_new_c},200
+
+    @v2.doc( security='apikey')
+    @jwt_required
+    @admin_required
+    def delete(self,id):
+        cur.execute("SELECT * FROM categories WHERE id={};".format(id))
+        category = cur.fetchone()
+        store_id = get_store_id(get_jwt_identity())
+        if not category or category[1] != store_id:
+            msg = {"message":'Category does not exist'},404
+            return msg
+        cur.execute("DELETE FROM  categories WHERE id={};".format(id))
+        conn.commit()
+        format_c = {
+            "category_name":category[2]
+        }
+        return {"status":"Deleted!","prpduct":format_c},200
+
+@v2.route('/<int:c_id>/products/<int:p_id>')
+class ProductCategory(Resource):
+
+    @v2.doc( security='apikey')
+    @jwt_required
+    @admin_required
+    def post(self,c_id,p_id):
+        """
+        Add a category to a product
+        c_id : the category id
+        p_id : the product id
+        """
+        store_id = get_store_id(get_jwt_identity())
+        cur.execute("SELECT * FROM categories WHERE id='{}';".format(c_id))
+        category = cur.fetchone()
+        if not category or category[1] != store_id:
+            msg = 'Category does not exist'
+            abort(406, msg)
+        cur.execute("SELECT * FROM products WHERE id={};".format(p_id))
+        product = cur.fetchone()
+        if not product or product[1] != store_id:
+            msg = 'Product does not exist'
+            abort(404,msg)
+        category_name = category[2]
+        cur.execute("UPDATE products SET category={0} WHERE id ={1}".format(category_name,p_id))
+        conn.commit()
+        cur.execute("SELECT * FROM products WHERE id={};".format(p_id))
+        new_p = cur.fetchone()
+        format_new_p ={
+            "product_name":new_p[2],
+            "inventory":new_p[3],
+            "price":new_p[4],
+            'category':new_p[5],
+            'added_at':new_p[6]
+        }
+        return {"status":"Updated!","product":format_new_p},200
