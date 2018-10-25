4ecd21d63584ba54e397fab69313f7241ae7e6a9
kwanj-k <mwangikelvin23@.com>
2018-10-25 06:59:14 +0300

    [Feature #161404710] Format travis config

--- .travis.sh
+++
@@ -1,5 +0,0 @@
-export MODE='testing'
-export SECRET_KEY="secretkeymyfootiknowyoucanseeit"
-export FLASK_APP="run.py"
-export FLASK_DEBUG=1
-export Test_URL="dbname=tch3 user=kwanj password=4084theG"
