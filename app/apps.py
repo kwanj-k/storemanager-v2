7740e34fb6aad5a1a9d6c60aa69557bb7c2772d1
kwanj-k <mwangikelvin23@.com>
2018-10-30 19:56:28 +0300

    Fix its dangerous

--- app/apps.py
+++ app/apps.py
@@ -29,7 +29,6 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     app.url_map.strict_slashes = False
-    #drop_all()
     create_tables()
     app.config['JWT_SECRET_KEY'] = secret_key
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=72)
